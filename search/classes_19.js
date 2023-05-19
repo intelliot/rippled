84b81d874dd3406291dab947abebd0ff973024d1
intelliot <intelliot@users.noreply.github.com>
2023-05-19 21:45:48 +0000

    deploy: 3620ac287e5a0b7206504a2032a6d4fb185bcf97

--- search/classes_19.js
+++ search/classes_19.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['yocto_15468',['yocto',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]],
-  ['yotta_15469',['yotta',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]]
+  ['yocto_15494',['yocto',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]],
+  ['yotta_15495',['yotta',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]]
 ];
